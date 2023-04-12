11c727fccf9e655fbd3c4ed449e13c58ea05fd38
Bit Tired <bbloldd@users.noreply.github.com>
2023-04-12 19:20:11 +0900

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,350 @@
+
+// For the chatsonic card
+var chatsonicLikeButton = document.getElementById("chatsonic-like-button");
+var chatsonicLikeCount = 0;
+var chatsonicLikeCountSpan = document.querySelector("#chatsonic-like-button span");
+chatsonicLikeButton.addEventListener("click", function() {
+  chatsonicLikeCount++;
+  chatsonicLikeCountSpan.innerHTML = chatsonicLikeCount;
+});
+
+var chatsonicDislikeButton = document.getElementById("chatsonic-dislike-button");
+var chatsonicDislikeCount = 0;
+var chatsonicDislikeCountSpan = document.querySelector("#chatsonic-dislike-button span");
+chatsonicDislikeButton.addEventListener("click", function() {
+  chatsonicDislikeCount++;
+  chatsonicDislikeCountSpan.innerHTML = chatsonicDislikeCount;
+});
+
+
+var likeCount = 0;
+var dislikeCount = 0;
+// For the AskUp card
+var askupLikeButton = document.getElementById("askup-like-button");
+var askupLikeCount = 0;
+var askupLikeCountSpan = document.querySelector("#askup-like-button span");
+askupLikeButton.addEventListener("click", function() {
+  askupLikeCount++;
+  askupLikeCountSpan.innerHTML = askupLikeCount;
+});
+
+var askupDislikeButton = document.getElementById("askup-dislike-button");
+var askupDislikeCount = 0;
+var askupDislikeCountSpan = document.querySelector("#askup-dislike-button span");
+askupDislikeButton.addEventListener("click", function() {
+  askupDislikeCount++;
+  askupDislikeCountSpan.innerHTML = askupDislikeCount;
+});
+
+
+var likeCount = 0;
+var dislikeCount = 0;
+// For the jasper card
+var jasperLikeButton = document.getElementById("jasper-like-button");
+var jasperLikeCount = 0;
+var jasperLikeCountSpan = document.querySelector("#jasper-like-button span");
+jasperLikeButton.addEventListener("click", function() {
+  jasperLikeCount++;
+  jasperLikeCountSpan.innerHTML = jasperLikeCount;
+});
+
+var jasperDislikeButton = document.getElementById("jasper-dislike-button");
+var jasperDislikeCount = 0;
+var jasperDislikeCountSpan = document.querySelector("#jasper-dislike-button span");
+jasperDislikeButton.addEventListener("click", function() {
+  jasperDislikeCount++;
+  jasperDislikeCountSpan.innerHTML = jasperDislikeCount;
+});
+
+
+var likeCount = 0;
+var dislikeCount = 0;
+// For the youchat card
+var youchatLikeButton = document.getElementById("youchat-like-button");
+var youchatLikeCount = 0;
+var youchatLikeCountSpan = document.querySelector("#youchat-like-button span");
+youchatLikeButton.addEventListener("click", function() {
+  youchatLikeCount++;
+  youchatLikeCountSpan.innerHTML = youchatLikeCount;
+});
+
+var youchatDislikeButton = document.getElementById("youchat-dislike-button");
+var youchatDislikeCount = 0;
+var youchatDislikeCountSpan = document.querySelector("#youchat-dislike-button span");
+youchatDislikeButton.addEventListener("click", function() {
+  youchatDislikeCount++;
+  youchatDislikeCountSpan.innerHTML = youchatDislikeCount;
+});
+
+var likeCount = 0;
+var dislikeCount = 0;
+// For the Socratic card
+var socraticLikeButton = document.getElementById("socratic-like-button");
+var socraticLikeCount = 0;
+var socraticLikeCountSpan = document.querySelector("#socratic-like-button span");
+socraticLikeButton.addEventListener("click", function() {
+  socraticLikeCount++;
+  socraticLikeCountSpan.innerHTML = socraticLikeCount;
+});
+
+var socraticDislikeButton = document.getElementById("socratic-dislike-button");
+var socraticDislikeCount = 0;
+var socraticDislikeCountSpan = document.querySelector("#socratic-dislike-button span");
+socraticDislikeButton.addEventListener("click", function() {
+  socraticDislikeCount++;
+  socraticDislikeCountSpan.innerHTML = socraticDislikeCount;
+});
+
+
+var likeCount = 0;
+var dislikeCount = 0;
+// For the youchat card
+var bardLikeButton = document.getElementById("bard-like-button");
+var bardLikeCount = 0;
+var bardLikeCountSpan = document.querySelector("#bard-like-button span");
+bardLikeButton.addEventListener("click", function() {
+  bardLikeCount++;
+  bardLikeCountSpan.innerHTML = bardLikeCount;
+});
+
+var bardDislikeButton = document.getElementById("bard-dislike-button");
+var bardDislikeCount = 0;
+var bardDislikeCountSpan = document.querySelector("#bard-dislike-button span");
+bardDislikeButton.addEventListener("click", function() {
+  bardDislikeCount++;
+  bardDislikeCountSpan.innerHTML = bardDislikeCount;
+});
+
+var likeCount = 0;
+var dislikeCount = 0;
+// For the tidio card
+var tidioLikeButton = document.getElementById("tidio-like-button");
+var tidioLikeCount = 0;
+var tidioLikeCountSpan = document.querySelector("#tidio-like-button span");
+tidioLikeButton.addEventListener("click", function() {
+  tidioLikeCount++;
+  tidioLikeCountSpan.innerHTML = tidioLikeCount;
+});
+
+var tidioDislikeButton = document.getElementById("tidio-dislike-button");
+var tidioDislikeCount = 0;
+var tidioDislikeCountSpan = document.querySelector("#tidio-dislike-button span");
+tidioDislikeButton.addEventListener("click", function() {
+  tidioDislikeCount++;
+  tidioDislikeCountSpan.innerHTML = tidioDislikeCount;
+});
+
+var likeCount = 0;
+var dislikeCount = 0;
+// For the drift card
+var driftLikeButton = document.getElementById("drift-like-button");
+var driftLikeCount = 0;
+var driftLikeCountSpan = document.querySelector("#drift-like-button span");
+driftLikeButton.addEventListener("click", function() {
+  driftLikeCount++;
+  driftLikeCountSpan.innerHTML = driftLikeCount;
+});
+
+var driftDislikeButton = document.getElementById("drift-dislike-button");
+var driftDislikeCount = 0;
+var driftDislikeCountSpan = document.querySelector("#drift-dislike-button span");
+driftDislikeButton.addEventListener("click", function() {
+  driftDislikeCount++;
+  driftDislikeCountSpan.innerHTML = driftDislikeCount;
+});
+
+var likeCount = 0;
+var dislikeCount = 0;
+// For the ada card
+var adaLikeButton = document.getElementById("ada-like-button");
+var adaLikeCount = 0;
+var adaLikeCountSpan = document.querySelector("#ada-like-button span");
+adaLikeButton.addEventListener("click", function() {
+  adaLikeCount++;
+  adaLikeCountSpan.innerHTML = adaLikeCount;
+});
+
+var adaDislikeButton = document.getElementById("ada-dislike-button");
+var adaDislikeCount = 0;
+var adaDislikeCountSpan = document.querySelector("#ada-dislike-button span");
+adaDislikeButton.addEventListener("click", function() {
+  adaDislikeCount++;
+  adaDislikeCountSpan.innerHTML = adaDislikeCount;
+});
+
+var likeCount = 0;
+var dislikeCount = 0;
+// For the dialogflow card
+var diaLikeButton = document.getElementById("dia-like-button");
+var diaLikeCount = 0;
+var diaLikeCountSpan = document.querySelector("#dia-like-button span");
+diaLikeButton.addEventListener("click", function() {
+  diaLikeCount++;
+  diaLikeCountSpan.innerHTML = diaLikeCount;
+});
+
+var diaDislikeButton = document.getElementById("dia-dislike-button");
+var diaDislikeCount = 0;
+var diaDislikeCountSpan = document.querySelector("#dia-dislike-button span");
+diaDislikeButton.addEventListener("click", function() {
+  diaDislikeCount++;
+  diaDislikeCountSpan.innerHTML = diaDislikeCount;
+});
+
+
+var likeCount = 0;
+var dislikeCount = 0;
+// For the wati card
+var watLikeButton = document.getElementById("wat-like-button");
+var watLikeCount = 0;
+var watLikeCountSpan = document.querySelector("#wat-like-button span");
+watLikeButton.addEventListener("click", function() {
+  watLikeCount++;
+  watLikeCountSpan.innerHTML = watLikeCount;
+});
+
+var watDislikeButton = document.getElementById("wat-dislike-button");
+var watDislikeCount = 0;
+var watDislikeCountSpan = document.querySelector("#wat-dislike-button span");
+watDislikeButton.addEventListener("click", function() {
+  watDislikeCount++;
+  watDislikeCountSpan.innerHTML = watDislikeCount;
+});
+
+
+var likeCount = 0;
+var dislikeCount = 0;
+// For the mobile monkey card
+var mobLikeButton = document.getElementById("mob-like-button");
+var mobLikeCount = 0;
+var mobLikeCountSpan = document.querySelector("#mob-like-button span");
+mobLikeButton.addEventListener("click", function() {
+  mobLikeCount++;
+  mobLikeCountSpan.innerHTML = mobLikeCount;
+});
+
+var mobDislikeButton = document.getElementById("mob-dislike-button");
+var mobDislikeCount = 0;
+var mobDislikeCountSpan = document.querySelector("#mob-dislike-button span");
+mobDislikeButton.addEventListener("click", function() {
+  mobDislikeCount++;
+  mobDislikeCountSpan.innerHTML = mobDislikeCount;
+});
+
+var likeCount = 0;
+var dislikeCount = 0;
+// For the paradox card
+var parLikeButton = document.getElementById("par-like-button");
+var parLikeCount = 0;
+var parLikeCountSpan = document.querySelector("#par-like-button span");
+parLikeButton.addEventListener("click", function() {
+  parLikeCount++;
+  parLikeCountSpan.innerHTML = parLikeCount;
+});
+
+var parDislikeButton = document.getElementById("par-dislike-button");
+var parDislikeCount = 0;
+var parDislikeCountSpan = document.querySelector("#par-dislike-button span");
+parDislikeButton.addEventListener("click", function() {
+  parDislikeCount++;
+  parDislikeCountSpan.innerHTML = parDislikeCount;
+});
+
+var likeCount = 0;
+var dislikeCount = 0;
+// For the infeedo card
+var infLikeButton = document.getElementById("inf-like-button");
+var infLikeCount = 0;
+var infLikeCountSpan = document.querySelector("#inf-like-button span");
+infLikeButton.addEventListener("click", function() {
+  infLikeCount++;
+  infLikeCountSpan.innerHTML = infLikeCount;
+});
+
+var infDislikeButton = document.getElementById("inf-dislike-button");
+var infDislikeCount = 0;
+var infDislikeCountSpan = document.querySelector("#inf-dislike-button span");
+infDislikeButton.addEventListener("click", function() {
+  infDislikeCount++;
+  infDislikeCountSpan.innerHTML = infDislikeCount;
+});
+
+
+var likeCount = 0;
+var dislikeCount = 0;
+// For the kaisisto card
+var kaiLikeButton = document.getElementById("kai-like-button");
+var kaiLikeCount = 0;
+var kaiLikeCountSpan = document.querySelector("#kai-like-button span");
+kaiLikeButton.addEventListener("click", function() {
+  kaiLikeCount++;
+  kaiLikeCountSpan.innerHTML = kaiLikeCount;
+});
+
+var kaiDislikeButton = document.getElementById("kai-dislike-button");
+var kaiDislikeCount = 0;
+var kaiDislikeCountSpan = document.querySelector("#kai-dislike-button span");
+kaiDislikeButton.addEventListener("click", function() {
+  kaiDislikeCount++;
+  kaiDislikeCountSpan.innerHTML = kaiDislikeCount;
+});
+
+
+var likeCount = 0;
+var dislikeCount = 0;
+// For the medwhat card
+var medLikeButton = document.getElementById("med-like-button");
+var medLikeCount = 0;
+var medLikeCountSpan = document.querySelector("#med-like-button span");
+medLikeButton.addEventListener("click", function() {
+  medLikeCount++;
+  medLikeCountSpan.innerHTML = medLikeCount;
+});
+
+var medDislikeButton = document.getElementById("med-dislike-button");
+var medDislikeCount = 0;
+var medDislikeCountSpan = document.querySelector("#med-dislike-button span");
+medDislikeButton.addEventListener("click", function() {
+  medDislikeCount++;
+  medDislikeCountSpan.innerHTML = medDislikeCount;
+});
+
+
+var likeCount = 0;
+var dislikeCount = 0;
+// For the replika card
+var repLikeButton = document.getElementById("rep-like-button");
+var repLikeCount = 0;
+var repLikeCountSpan = document.querySelector("#rep-like-button span");
+repLikeButton.addEventListener("click", function() {
+  repLikeCount++;
+  repLikeCountSpan.innerHTML = repLikeCount;
+});
+
+var repDislikeButton = document.getElementById("rep-dislike-button");
+var repDislikeCount = 0;
+var repDislikeCountSpan = document.querySelector("#rep-dislike-button span");
+repDislikeButton.addEventListener("click", function() {
+  repDislikeCount++;
+  repDislikeCountSpan.innerHTML = repDislikeCount;
+});
+
+
+var likeCount = 0;
+var dislikeCount = 0;
+// For the Elomia Health card
+var eloLikeButton = document.getElementById("elo-like-button");
+var eloLikeCount = 0;
+var eloLikeCountSpan = document.querySelector("#elo-like-button span");
+eloLikeButton.addEventListener("click", function() {
+  eloLikeCount++;
+  eloLikeCountSpan.innerHTML = eloLikeCount;
+});
+
+var eloDislikeButton = document.getElementById("elo-dislike-button");
+var eloDislikeCount = 0;
+var eloDislikeCountSpan = document.querySelector("#elo-dislike-button span");
+eloDislikeButton.addEventListener("click", function() {
+  eloDislikeCount++;
+  eloDislikeCountSpan.innerHTML = eloDislikeCount;
+});
